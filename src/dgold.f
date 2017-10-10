5e3c26e2dc8f82a3c202ab7d9d5c120d0644d9aa
Tayler Blake <tayler.a.blake@gmail.com>
2017-10-09 21:42:39 -0400

    add helper subroutine to decompose q(lambda) within dgold

--- src/dgold.f
+++ src/dgold.f
@@ -1,15 +1,16 @@
 C Output from Public domain Ratfor, version 1.0
       subroutine  dgold (vmu, s, lds, qraux, nobs, nnull, tol, jpvt, M,
-     *ldmr, ldmc, nq, q, ldqr, ldqc, z, y, low, upp, nlaht, score,
+     *ldq, nq, q, z, y, low, upp, nlaht, score,
      *varht,info, twk, twk2, work, qwork)
 C Varible declares
       character         vmu
-      integer           ldmr, ldmc, ldqr, ldqc, info, jpvt(*), lds,
+      integer           ldq, info, jpvt(*), lds,
      *nnull, nobs, ncol, nq
-      double precision  q(ldqr,ldqc,*), M(ldmr,ldmc,*), tol, z(*), y(*),
+      double precision  q(ldq,*), M(ldm,*), tol, z(*), y(*),
      *low, upp, nlaht, score, varht, twk(2,*), twk2(*), work(*),
-     *qwork(ldmr,ldmc,*), qraux(*)
+     *qwork(ldq,*), qraux(*)
       double precision ratio, mlo, mup, tmpl, tmpu
+
 C Begin the code
       ratio = ( dsqrt (5.d0) - 1.d0 ) / 2.d0
       n0 = nnull
@@ -29,49 +30,40 @@ C Begin the code
       return
       endif
       mlo = upp - ratio * (upp - low)
-      do j=1,nq,1
-      call dcopy(ldmr*ldmc,M,1,qwork,1)
-      call dscal(ldmr*ldmc,mlo,qwork,1)
-      call daxpy(ldq*ldq,1,q(1,1,j),1,qwork,1)
-      call dqrslm (s, lds, nobs, nnull, qraux, qwork(1,1,j), ldmr, 0,
-     *info, work)
-      end do
 
-      call  dsytr (qwork(n0+1,n0+1), ldmr, ncol, tol, info, work)
+      call dgstup ( s, M, lds, nobs, nnull, qraux, q, ldq, nobs,
+     *nq, info, work, qwork, mlo)
+
+      call  dsytr (qwork(n0+1,n0+1), ldq, ncol, tol, info, work)
       if ( info .ne. 0 )  return
       call  dcopy (ncol-2, q(n0+2,n0+1), ldq+1, work, 1)
-      call  dqrsl (qwork(n0+2,n0+1), ldq, ncol-1, ncol-2,  work, y(n0+2),
-     *dum, z(n0+2),dum, dum, dum, 01000, info)
+      call  dqrsl (qwork(n0+2,n0+1), ldq, ncol-1, ncol-2, work,
+     *y(n0+2),dum, z(n0+2),dum, dum, dum, 01000, info)
       call  dset (ncol, 0.d0, twk(2,1), 2)
       call  daxpy (ncol, 1.d0, q, ldq+1, twk(2,1), 2)
       call  dcopy (ncol-1, qwork(1,2), ldq+1, twk(1,2), 2)
       twk(1,1) = 10.d0**mlo
-      call  dtrev (vmu, twk, 2, M, nobs, ncol, z, mlo, varht, info,
-     *work, twk2)
+      call  dtrev (vmu, twk, 2, M, nobs, ncol, z, mlo, varht,
+     *info,work, twk2)
       if( info .ne. 0 )then
       info = -2
       return
       endif
       mup = low + ratio * (upp - low)
-      do j=1,nq,1
-      call dcopy(ldmr*ldmc,M,1,qwork,1)
-      call dscal(ldmr*ldmc,mup,qwork,1)
-      call daxpy(ldq*ldq,1,q(1,1,j),1,qwork,1)
-      call dqrslm (s, lds, nobs, nnull, qraux, qwork(1,1,j), ldmr, 0,
-     *info, work)
-      end do
+      call dgstup ( s, M, lds, nobs, nnull, qraux, q, ldq, nobs,
+     *nq, info, work, qwork, mup)
 
-      call  dsytr (qwork(n0+1,n0+1), ldmr, ncol, tol, info, work)
+      call  dsytr (qwork(n0+1,n0+1), ldq, ncol, tol, info, work)
       if ( info .ne. 0 )  return
       call  dcopy (ncol-2, q(n0+2,n0+1), ldq+1, work, 1)
-      call  dqrsl (qwork(n0+2,n0+1), ldq, ncol-1, ncol-2,  work, y(n0+2),
-     *dum, z(n0+2),dum, dum, dum, 01000, info)
+      call  dqrsl (qwork(n0+2,n0+1), ldq, ncol-1, ncol-2,  work,
+     *y(n0+2), dum, z(n0+2),dum, dum, dum, 01000, info)
       call  dset (ncol, 0.d0, twk(2,1), 2)
       call  daxpy (ncol, 1.d0, q, ldq+1, twk(2,1), 2)
       call  dcopy (ncol-1, qwork(1,2), ldq+1, twk(1,2), 2)
       twk(1,1) = 10.d0**mup
-      call  dtrev (vmu, twk, 2, M, nobs, ncol, z, mup, varht, info,
-     *work, twk2)
+      call  dtrev (vmu, twk, 2, M, nobs, ncol, z, mup, varht,
+     *info, work, twk2)
       if( info .ne. 0 )then
       info = -2
       return
@@ -85,15 +77,10 @@ C Begin the code
       mup = mlo
       tmpu = tmpl
       mlo = upp - ratio * (upp - low)
-      do j=1,nq,1
-      call dcopy(ldmr*ldmc,M,1,qwork,1)
-      call dscal(ldmr*ldmc,mlo,qwork,1)
-      call daxpy(ldq*ldq,1,q(1,1,j),1,qwork,1)
-      call dqrslm (s, lds, nobs, nnull, qraux, qwork(1,1,j), ldmr, 0,
-     *info, work)
-      end do
+      call dgstup ( s, M, lds, nobs, nnull, qraux, q, ldq, nobs,
+     *nq, info, work, qwork, mlo)
 
-      call  dsytr (qwork(n0+1,n0+1), ldmr, ncol, tol, info, work)
+      call  dsytr (qwork(n0+1,n0+1), ldq, ncol, tol, info, work)
       if ( info .ne. 0 )  return
       call  dcopy (ncol-2, q(n0+2,n0+1), ldq+1, work, 1)
       call  dqrsl (qwork(n0+2,n0+1), ldq, ncol-1, ncol-2,  work,
@@ -112,19 +99,14 @@ C Begin the code
       mlo = mup
       tmpl = tmpu
       mup = low + ratio * (upp - low)
-      do j=1,nq,1
-      call dcopy(ldmr*ldmc,M,1,qwork,1)
-      call dscal(ldmr*ldmc,mup,qwork,1)
-      call daxpy(ldq*ldq,1,q(1,1,j),1,qwork,1)
-      call dqrslm (s, lds, nobs, nnull, qraux, qwork(1,1,j), ldmr, 0,
-     *info, work)
-      end do
+      call dgstup ( s, M, lds, nobs, nnull, qraux, q, ldq, nobs,
+     *nq, info, work, qwork, mlo)
 
-      call  dsytr (qwork(n0+1,n0+1), ldmr, ncol, tol, info, work)
+      call  dsytr (qwork(n0+1,n0+1), ldq, ncol, tol, info, work)
       if ( info .ne. 0 )  return
       call  dcopy (ncol-2, q(n0+2,n0+1), ldq+1, work, 1)
-      call  dqrsl (qwork(n0+2,n0+1), ldq, ncol-1, ncol-2,  work, y(n0+2),
-     *dum, z(n0+2),dum, dum, dum, 01000, info)
+      call  dqrsl (qwork(n0+2,n0+1), ldq, ncol-1, ncol-2,  work,
+     *y(n0+2), dum, z(n0+2),dum, dum, dum, 01000, info)
       call  dset (ncol, 0.d0, twk(2,1), 2)
       call  daxpy (ncol, 1.d0, q, ldq+1, twk(2,1), 2)
       call  dcopy (ncol-1, qwork(1,2), ldq+1, twk(1,2), 2)
@@ -138,19 +120,14 @@ C Begin the code
 23011 goto 23010
 23012 continue
       nlaht = ( mup + mlo ) / 2.d0
-      do j=1,nq,1
-      call dcopy(ldmr*ldmc,M,1,qwork,1)
-      call dscal(ldmr*ldmc,nlaht,qwork,1)
-      call daxpy(ldq*ldq,1,q(1,1,j),1,qwork,1)
-      call dqrslm (s, lds, nobs, nnull, qraux, qwork(1,1,j), ldmr, 0,
-     *info, work)
-      end do
+      call dgstup ( s, M, lds, nobs, nnull, qraux, q, ldq, nobs,
+     *nq, info, work, qwork, mlo)
 
-      call  dsytr (qwork(n0+1,n0+1), ldmr, ncol, tol, info, work)
+      call  dsytr (qwork(n0+1,n0+1), ldq, ncol, tol, info, work)
       if ( info .ne. 0 )  return
       call  dcopy (ncol-2, q(n0+2,n0+1), ldq+1, work, 1)
-      call  dqrsl (qwork(n0+2,n0+1), ldq, ncol-1, ncol-2,  work, y(n0+2),
-     *dum, z(n0+2),dum, dum, dum, 01000, info)
+      call  dqrsl (qwork(n0+2,n0+1), ldq, ncol-1, ncol-2,  work,
+     *y(n0+2), dum, z(n0+2),dum, dum, dum, 01000, info)
       call  dset (ncol, 0.d0, twk(2,1), 2)
       call  daxpy (ncol, 1.d0, q, ldq+1, twk(2,1), 2)
       call  dcopy (ncol-1, qwork(1,2), ldq+1, twk(1,2), 2)
---
+++ src/dgstup.f
@@ -0,0 +1,25 @@
+      subroutine dgstup (s, M, ldm, lds, nobs, nnull, qraux, q, ldqr,
+     *ldqc, nq, info, work, qwk, nla)
+
+      integer lds, nobs, nnull, ldqr, ldqc, nq, info, ldm
+      double precision s(lds,*), qraux(*), q(ldqr,ldqc,*),
+     *work(*), q(ldqr,ldqc,*), M(ldqr,ldqc,*)
+      double precision dum, nla
+      integer j
+      info = 0
+
+      if ( nobs .lt. 1 .or. nobs .gt. lds .or. nobs .gt. ldqr .or. nobs
+     *.gt. ldqc )then
+      info = -1
+      return
+      endif
+
+      do j=1,nq,1
+      call dcopy(ldqr*ldqc,M,1,q,1)
+      call dscal(ldqr*ldqc,nla,q,1)
+      call daxpy(ldqr*ldqc,1,q(1,1,j),1,q,1)
+      call dqrslm(s, lds, nobs, nnull, qraux, q(1,1,j), ldqr, 0,
+     *info, work)
+      end do
+      return
+      end
